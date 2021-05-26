17ebea4cfa9fe71c0d64deb1ec4e74f241e61a7e
ArashIP <arashmahmoodi665@gmail.com>
2021-05-26 09:46:10 +0430

    Dark and light mood
    Set dark mood and light mood button and do some changes

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const hamburger = document.getElementById('ham-btn')
 const sidebar = document.getElementById('side')
-let isclick = false
+const themeBtn = document.getElementById('theme-btn')
+const texttheme = document.querySelector('#theme-btn span')
+const i = document.getElementById('i')
+
+let clicked = false
+let clickedtheme = false
+
 
 hamburger.addEventListener('click',() => {
-    if(isclick == false){
+    if(clicked == false){
         hamburger.classList.add('open')
         sidebar.classList.add('sidebar')
         sidebar.classList.remove('sidebarclose')
-        isclick = true
+        clicked = true
     }else{
         hamburger.classList.remove('open')
         sidebar.classList.remove('sidebar')
         sidebar.classList.add('sidebarclose')
-        isclick = false
+        clicked = false
     }
+})
 
-    
-    
+i.classList.add('fas')
+i.classList.add('fa-sun')
+
+themeBtn.addEventListener('click',() => {
+    if(clickedtheme == false){
+        themeBtn.classList.remove('btn-dark')
+        themeBtn.classList.add('btn-light')
+        i.classList.replace('fa-sun','fa-moon')
+        texttheme.textContent = "Dark mood"
+        clickedtheme = true
+    }else{
+        themeBtn.classList.add('btn-dark')
+        themeBtn.classList.remove('btn-light')
+        i.classList.replace('fa-moon','fa-sun')
+        texttheme.textContent = "Light mood"
+        clickedtheme = false
+    }
 })
