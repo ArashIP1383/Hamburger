981af46ffedbe965d9c113bd0a7f5276333f87d7
ArashIP <arashmahmoodi665@gmail.com>
2021-05-29 13:51:32 +0430

    Add changeable theme feature and complete media quarry

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const hamburger = document.getElementById('ham-btn')
 const sidebar = document.getElementById('side')
 const themeBtn = document.getElementById('theme-btn')
-const texttheme = document.querySelector('#theme-btn span')
+const texttheme = document.querySelector('#theme-btn p')
 const i = document.getElementById('i')
+const style = document.getElementById('style')
+
 
 let clicked = false
 let clickedtheme = false
@@ -23,20 +25,22 @@ hamburger.addEventListener('click',() => {
 })
 
 i.classList.add('fas')
-i.classList.add('fa-sun')
+i.classList.add('fa-moon')
 
 themeBtn.addEventListener('click',() => {
     if(clickedtheme == false){
         themeBtn.classList.remove('btn-dark')
         themeBtn.classList.add('btn-light')
         i.classList.replace('fa-sun','fa-moon')
-        texttheme.textContent = "Dark mood"
+        texttheme.textContent = "Light mood"
+        style.setAttribute('href','style-dark.css')
         clickedtheme = true
     }else{
         themeBtn.classList.add('btn-dark')
         themeBtn.classList.remove('btn-light')
         i.classList.replace('fa-moon','fa-sun')
-        texttheme.textContent = "Light mood"
+        texttheme.textContent = "Dark mood"
+        style.setAttribute('href','style-light.css')
         clickedtheme = false
     }
 })
